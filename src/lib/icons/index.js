ed37caf1d5cd609b83c228fc8cf1615a1f897627
moaqz <goz.matu@gmail.com>
2023-08-02 18:24:47 -0300

    feat: add icons

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -1,7 +1,15 @@
 import RestartIcon from './restart.svelte'
 import PointerIcon from './pointer.svelte'
+import CrownIcon from './crown.svelte';
+import UserIcon from './user.svelte';
+import InfoIcon from './info.svelte';
+import CodeIcon from './code.svelte';
 
 export {
   RestartIcon,
   PointerIcon,
+  CrownIcon,
+  UserIcon,
+  InfoIcon,
+  CodeIcon,
 };
